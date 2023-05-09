c36b892387cb958533968e79006d4b8fc5029ba3
Fanger53 <fanger53@gmail.com>
2023-05-09 10:11:51 -0500

    add todowrapper component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.scss';
 
 import React from 'react'
+import TodoWrapper from './components/wrapper/TodoWrapper';
 
 const App = () => {
   return (
     <>
+      <TodoWrapper/>
     </>
   )
 }
---
+++ src/components/wrapper/TodoWrapper.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TodoWrapper.scss';
+
+const TodoWrapper = () => {
+  return (
+    <div className='todoWrapper border'>
+      <h2>Work List</h2>
+    </div>
+  )
+}
+
+export default TodoWrapper
